735d1566292113680e7d96dc126c61f6752f2ffa
PieterDePauw <pieter@pieterdepauw.com>
2023-05-12 19:34:15 +0200

    DropDown updates

--- components/Common/DropDown.jsx
+++ components/Common/DropDown.jsx
@@ -33,27 +33,25 @@ function DropDown({menuList, marginPos, top, setOpen, open, buttonRef}) {
         addBox.style.zIndex = 1;
     }
 
-
-
     return (<>
-        <div ref={ref} className={clsx('dropdown', open ? 'active' : null)}
-             style={{transform: `translate(${dropdownX}px, ${top}px)`}}>
-            {menuList.length !== 0 ? <ul className='dropdown_list'>
-                {menuList.map((elem, index) => {
-                    return <li key={index} className='dropdown_item' onClick={() => {
-                        elem.onClick()
-                        setOpen(false)
-                        addBox.style.zIndex = 5;
-                    }}>
-                        <div className="dropdown_item--icon">
-                            {elem.icon}
-                        </div>
-                        <div className="dropdown_item--content">
-                            {elem.name}
-                        </div>
-                    </li>
-                })}
-            </ul> : null}
+        <div ref={ref} className={clsx('dropdown', open ? 'active' : null)} style={{transform: `translate(${dropdownX}px, ${top}px)`}}>
+            {menuList.length !== 0 ?
+                <ul className='dropdown_list'>
+                    {menuList.map((elem, index) => {
+                        return <li key={index} className='dropdown_item' onClick={() => {
+                            elem.onClick()
+                            setOpen(false)
+                            addBox.style.zIndex = 5;
+                        }}>
+                            <div className="dropdown_item--icon">
+                                {elem.icon}
+                            </div>
+                            <div className="dropdown_item--content">
+                                {elem.name}
+                            </div>
+                        </li>
+                    })}
+                </ul> : null}
         </div>
     </>);
 }
