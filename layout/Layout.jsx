8e7633d3f642156d110bb9710fe7998e4d328898
PieterDePauw <pieter@pieterdepauw.com>
2023-05-12 19:11:27 +0200

    Layout.jsx

--- layout/Layout.jsx
+++ layout/Layout.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import AppBar from "../components/Navigation/AppBar";
 
 function Layout({ children }) {
-    return (<>
-        <AppBar/>
-        <main>{children}</main>
-    </>);
+    return (
+        <>
+            <AppBar/>
+            <main>
+                {children}
+            </main>
+        </>
+    );
 }
 
 export default Layout;
