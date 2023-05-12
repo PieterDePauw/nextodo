9712de429b3c83dc82f24a5152d419659e7f5fc8
PieterDePauw <pieter@pieterdepauw.com>
2023-05-12 19:32:31 +0200

    App, Document and Index updates

--- pages/_document.js
+++ pages/_document.js
@@ -1,36 +1,23 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { CssBaseline } from '@nextui-org/react'
+import { CssBaseline } from '@mui/material'
 
-
-
-class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        const styles = CssBaseline.flush()
-
-        return {
-            ...initialProps,
-            styles: (
-                <>
-                    {initialProps.styles}
-                    {styles}
-                </>
-            ),
-        }
-    }
-
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
+function MyDocument() {
+    return (
+        <Html lang="en">
+            <Head />
+            <body>
                 <Main />
                 <NextScript />
-                </body>
-            </Html>
-        )
-    }
+            </body>
+        </Html>
+    )
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = CssBaseline.flush()
+    return ({ ...initialProps, styles: ( <> {initialProps.styles} {styles} </> ) })
 }
 
 export default MyDocument
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import TodosApp from "../components/TodosApp";
 
 export default function Home() {
-    const handleAddTodo = () => {
-        console.log("add");
-    }
+
+/**********************************************
+****    const handleAddTodo = () => {      ****
+****        console.log("add");            ****
+****    }                                  ****
+**********************************************/
+
     return (
         <>
             <TodosApp/>
